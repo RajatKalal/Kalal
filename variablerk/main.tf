4943ad030aaf190d3618da2f8309985ee352a549
RajatKalal <rajatkarnwal1704@gmail.com>
2025-05-15 15:28:42 +0530

    Update main.tf

--- variablerk/main.tf
+++ variablerk/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "var.variable_name"
-  location = "var.location"
+resource         "azurerm_resource_group" "rg" {
+  name     =                   "var.variable_name"
+  location =                     "var.location"
 }
